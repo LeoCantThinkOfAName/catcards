1935a7162cc3a8fa9548d1b58b00351210eaf579
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-04-21 22:58:16 +0800

    fix: remove warning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+
 import Cats from "./cat";
 
 function App() {
   return (
     <div className="App">
       {Cats.map((cat) => (
-        <div class="card">
-          <img class="card-img-top" src={cat.img} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">{cat.breed}</h5>
-            <h6 class="card-subtitle">
+        <div className="card" key={cat.breed}>
+          <img className="card-img-top" src={cat.img} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{cat.breed}</h5>
+            <h6 className="card-subtitle">
               {cat.color} {cat.sex} {cat.age}
             </h6>
-            <p class="card-text">{cat.desc}</p>
+            <p className="card-text">{cat.desc}</p>
           </div>
         </div>
       ))}
